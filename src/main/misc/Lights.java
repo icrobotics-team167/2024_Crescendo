12005b27c3755d1dd670bc0bbb6a5427f7769e06
Iowa City Robotics <info@iowacityrobotics.org>
2023-12-07 02:54:43 -0600

    GOD SAVE THE QUEEN

--- src/main/misc/Lights.java
+++ src/main/misc/Lights.java
@@ -1,15 +1,55 @@
 import edu.wpi.first.wpilibj.Spark;
 
+/**
+ * Lights class for cool lights bruv.
+*/
 public class Lights {
-    Spark colorSpark;
+    Spark colourSpark;
 
-    private static enum Colors {
-        
+    /**
+     * Cool colours bruv.
+     */
+    private static enum Colours {
+        // cool colours. NOT COLORS BRUV!!!!!!!
+        RAINBOWPALETTE,
+        RAINBOWGLITTER,
+        CONFETTI,
+        SHOTRED,
+        SINELONRAINBOXPALETTE,
+        SINELONLAVAPALETTE,
+        BPMOCEANPALETTE,
+        FIRELARGE,
+        TWINKLESPARTYPALETTE,
+        LARSONSCANNERGRAY,
+        LIGHTCHASEBLUE,
+        HEARTBEATBLUE,
+        BREATHGRAY,
+        STROBEGOLD,
+        HOTPINKSOLID,
+        DARKREDSOLID,
+        REDSOLID,
+        GOLDSOLID,
+        LAWNGREENSOLID,
+        LIMESOLID,
+        DARKGREENSOLID;
+
+        double colourValue;
+        /**
+         * Colours constructor 
+         * @param colourValue colour value bruv
+         */
+        private Colours(double colourValue) {
+            this.colorValue = colourValue;
+        }
     }
 
+    /**
+     * Loit contstructor bruv
+     * @param PWMID the PWMID of the blinky blinky light module bruv (Spark motor)
+     */ 
     public Light(int PWMID) {
-        colorSpark = new Spark(PWMID);
+        colourSpark = new Spark(PWMID);
 
-        colorSpark.set(-0.99);
+        colourSpark.set(-0.99);
     }
 }
