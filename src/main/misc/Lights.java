ec3d694430b552d2a6f9e5dcaa910f522ed53aaf
Iowa City Robotics <info@iowacityrobotics.org>
2023-12-07 02:35:54 -0600

    COLOUR STUFFFFF

--- src/main/misc/Lights.java
+++ src/main/misc/Lights.java
@@ -1,10 +1,15 @@
 import edu.wpi.first.wpilibj.Spark;
 
 public class Lights {
-    private static class Colors {
+    Spark colorSpark;
 
+    private static enum Colors {
+        
     }
 
     public Light(int PWMID) {
+        colorSpark = new Spark(PWMID);
+
+        colorSpark.set(-0.99);
     }
 }
