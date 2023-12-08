db98ceec4005d2e5ea42851d0a156d43e80f5691
Syatram <113729656+Syatram@users.noreply.github.com>
2023-12-08 11:14:32 -0600

    readded Gido's bri*ish bullshit. Also added underscore to the colour names.

--- src/main/misc/Lights.java
+++ src/main/misc/Lights.java
@@ -4,40 +4,40 @@ import edu.wpi.first.wpilibj.Spark;
  * Lights class for cool lights bruv.
 */
 public class Lights {
-    Spark colorspark;
+    Spark colourspark;
 
     /**
-     * Cool colors.
+     * Cool colours.
      */
-    private static enum Colors {
-        RAINBOWPALETTE(-0.99),
-        RAINBOWGLITTER(-0.89),
+    private static enum Colours {
+        RAINBOW_PALETTE(-0.99),
+        RAINBOW_GLITTER(-0.89),
         CONFETTI(-0.87),
-        SHOTRED(-0.85),
-        SINELONRAINBOXPALETTE(-0.79),
-        SINELONLAVAPALETTE(-0.73),
-        BPMOCEANPALETTE(-0.75),
-        FIRELARGE(-0.57),
-        TWINKLESPARTYPALETTE(-0.53),
-        LARSONSCANNERGRAY(-0.33),
-        LIGHTCHASEBLUE(-0.29),
-        HEARTBEATBLUE(-0.23),
-        BREATHGRAY(-0.13),
-        STROBEGOLD(-0.07),
-        HOTPINKSOLID(0.57),
-        DARKREDSOLID(0.59),
-        REDSOLID(0.61),
-        GOLDSOLID(0.67),
-        LAWNGREENSOLID(0.71),
-        LIMESOLID(0.73),
-        DARKGREENSOLID(0.75);
+        SHOT_RED(-0.85),
+        SINELON_RAINBOX_PALETTE(-0.79),
+        SINELON_LAVA_PALETTE(-0.73),
+        BPM_OCEAN_PALETTE(-0.75),
+        FIRE_LARGE(-0.57),
+        TWINKLES_PARTY_PALETTE(-0.53),
+        LARSON_SCANNER_GRAY(-0.33),
+        LIGHT_CHASEB_LUE(-0.29),
+        HEARTBEAT_BLUE(-0.23),
+        BREATH_GRAY(-0.13),
+        STROBE_GOLD(-0.07),
+        HOT_PINK_SOLID(0.57),
+        DARK_RED_SOLID(0.59),
+        RED_SOLID(0.61),
+        GOLD_SOLID(0.67),
+        LAWN_GREEN_SOLID(0.71),
+        LIME_SOLID(0.73),
+        DARK_GREEN_SOLID(0.75);
 
-        double colorValue;
+        double colourValue;
         /**
-         * colors constructor
+         * colours constructor
          * @param colourValue colour value
          */
-        private colors(double colourValue) {
+        private colours(double colourValue) {
             this.colourValue = colourValue;
         }
     }
@@ -47,15 +47,15 @@ public class Lights {
      * @param PWMID the PWMID of the blinky blinky light module (Spark motor)
      */ 
     public Light(int PWMID) {
-        colorspark = new Spark(PWMID);
+        colourspark = new Spark(PWMID);
     }
 
     /**
-     * Set the color for the spark motor bruv
-     * @param color colors object bruv
+     * Set the colour for the spark motor bruv
+     * @param colour colours object bruv
      */
-    public void setColour(colors color) {
+    public void setColour(colours colour) {
         // set colour to set colourValue bruv
-        colorspark.set(colour.colourValue);
+        colourspark.set(colour.colourValue);
     }
 }
