3875f97124268dd93b86de2318ee3502cb036511
TheComputer <thecomputer314@gmail.com>
2024-01-16 12:19:41 -0600

    Start work on intake

---
+++ src/main/java/frc/robot/commands/Intake.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class Intake extends Command {
+    ShooterSubsystem shooter;
+    public Intake(ShooterSubsystem shooter) {
+        this.shooter = shooter;
+        addRequirements(shooter);
+        setName("Intake");
+    }
+
+    @Override
+    public void execute() {
+        // TODO: Configure
+        shooter.setPivot(new Rotation2d());
+        shooter.runIntake();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shooter.hasRing();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooter.stopIntake();
+    }
+}
