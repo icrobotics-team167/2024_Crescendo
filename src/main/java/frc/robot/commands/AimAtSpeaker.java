03ef11054a6e2c838dda2ecb7fe4acea9de3b0a6
TheComputer <thecomputer314@gmail.com>
2024-01-22 19:04:11 -0600

    Reimplement autoaim

--- src/main/java/frc/robot/commands/AimAtSpeaker.java
+++ src/main/java/frc/robot/commands/AimAtSpeaker.java
@@ -1,30 +1,48 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleConsumer;
 import java.util.function.Supplier;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.Vision.LimeLight;
+import frc.robot.helpers.LimelightHelpers;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class AimAtSpeaker extends Command {
     ShooterSubsystem shooter;
-    Supplier<Pose2d> botPoseSupplier;
+    DoubleConsumer rotationalOverrideConsumer;
+    Runnable rotationalOverrideDisabler;
 
-    public AimAtSpeaker(ShooterSubsystem shooter, Supplier<Pose2d> botPoseSupplier) {
+    PIDController rotationalOverridePID = new PIDController(1.0 / 75.0, 0, 0);
+    PIDController pivotPID = new PIDController(0.9, 0, 0);
+
+    public AimAtSpeaker(ShooterSubsystem shooter, DoubleConsumer rotationalOverrideConsumer,
+            Runnable rotationalOverrideDisabler) {
         this.shooter = shooter;
-        this.botPoseSupplier = botPoseSupplier;
+        this.rotationalOverrideConsumer = rotationalOverrideConsumer;
+        this.rotationalOverrideDisabler = rotationalOverrideDisabler;
+
         addRequirements(shooter);
         setName("Aim At Speaker");
     }
 
     @Override
     public void initialize() {
-        
+
     }
 
     @Override
     public void execute() {
-        
+        if (!LimelightHelpers.getTV(LimeLight.APRILTAG_DETECTOR)) {
+            return;
+        }
+        double rotError = LimelightHelpers.getTX(LimeLight.APRILTAG_DETECTOR);
+        double pivotError = LimelightHelpers.getTY(LimeLight.APRILTAG_DETECTOR);
+
+        rotationalOverrideConsumer.accept(rotationalOverridePID.calculate(rotError, 0));
+        shooter.runPivot(pivotPID.calculate(pivotError, 0));
     }
 
     @Override
@@ -34,6 +52,6 @@ public class AimAtSpeaker extends Command {
 
     @Override
     public void end(boolean interrupted) {
-
+        rotationalOverrideDisabler.run();
     }
 }
