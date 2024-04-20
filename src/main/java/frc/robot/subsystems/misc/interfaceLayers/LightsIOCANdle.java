2a656ed97d73aad71ad4d89a6f127f751fb62bc6
Iowa City Robotics <info@iowacityrobotics.org>
2024-04-19 19:49:12 -0500

    The CANdle works now
    1) Set the number of LEDs, comment about how that number is used
    2) Changed some values in the animations to gain better ideas on how to control them
    
    - Spencer (Blame Tom)

--- src/main/java/frc/robot/subsystems/misc/interfaceLayers/LightsIOCANdle.java
+++ src/main/java/frc/robot/subsystems/misc/interfaceLayers/LightsIOCANdle.java
@@ -35,7 +35,9 @@ public class LightsIOCANdle implements LightsIO {
     candle.configAllSettings(config);
   }
 
-  private static final int LIGHT_NUM = 42;
+  // Ok so for some unbeknownst reason this number includes the 8 LEDs the CANdle actually has on
+  // board so i have left it being in the form of X + 8 where X is the number of LEDs on the strip
+  private static final int LIGHT_NUM = 36 + 8;
 
   @Override
   public void setColorFromState(LightState state) {
@@ -45,19 +47,21 @@ public class LightsIOCANdle implements LightsIO {
         animation = new RainbowAnimation(1, 1, LIGHT_NUM, false, 0);
       }
       case AIM_OK -> {
-        animation = new ColorFlowAnimation(0, 255, 0, 0, 1, LIGHT_NUM, Direction.Forward, 0);
+        //IF SPEED IS 1 IT STROBES
+        animation = new ColorFlowAnimation(0, 255, 0, 0, .6, LIGHT_NUM, Direction.Forward, 0);
       }
       case HAS_NOTE -> {
         animation = new TwinkleAnimation(0, 255, 0, 0, 1, LIGHT_NUM, TwinklePercent.Percent100, 0);
       }
       case INDEXING_NOTE -> {
-        animation = new StrobeAnimation(0, 255, 0, 0, 1, LIGHT_NUM, 0);
+        //IF SPEED IS 1 ITS A SOLID COLOR
+        animation = new StrobeAnimation(0, 255, 0, 0, .85, LIGHT_NUM, 0);
       }
       case INTAKING -> {
-        animation = new RgbFadeAnimation(1, 1, LIGHT_NUM, 0);
+        animation = new RgbFadeAnimation(1, .85, LIGHT_NUM, 0);
       }
       case NO_NOTE -> {
-        animation = new FireAnimation(1, .7, LIGHT_NUM, .8, .75);
+        animation = new FireAnimation(1, .7, LIGHT_NUM, .8, .2);
       }
       case SHOOTING -> {
         animation = new LarsonAnimation(0, 255, 0, 0, 1, LIGHT_NUM, BounceMode.Front, 2, 0);
@@ -70,6 +74,6 @@ public class LightsIOCANdle implements LightsIO {
   }
 
   public void setLEDTest() {
-    candle.setLEDs(255,0,0,0,8,3);
+    candle.setLEDs(255, 0, 0, 0, 8, 36);
   }
 }
